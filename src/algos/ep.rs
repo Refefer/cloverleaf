7723b27867774ce6a929b6b681e2a82c6d36db2d
Andrew Stanton <astanton@etsy.com>
2023-01-26 13:43:59 -0500

    updates to optimizer interface

--- src/algos/ep.rs
+++ src/algos/ep.rs
@@ -58,10 +58,9 @@ impl EmbeddingPropagation {
         };
 
         // We create separate embeddings for momentum and feature_embeddings.
-        let momentum = EmbeddingStore::new(
-            feature_embeddings.len(), 
+        let optimizer = MomentumOptimizer::new(self.gamma, 
             feature_embeddings.dims(), 
-            Distance::Cosine);
+            feature_embeddings.len()); 
 
         let mut node_idxs: Vec<_> = (0..graph.len()).into_iter().collect();
         let pb = CLProgressBar::new((self.passes * graph.len()) as u64, self.indicator);
@@ -105,7 +104,8 @@ impl EmbeddingPropagation {
                 }
                 
                 // Backpropagate embeddings
-                sgd(&feature_embeddings, &momentum, self.gamma, &mut all_grads, alpha);
+                optimizer.update(&feature_embeddings, &mut all_grads, alpha, pass as f32);
+                //sgd(&feature_embeddings, &momentum, self.gamma, &mut all_grads, alpha);
 
                 // Update progress bar
                 pb.inc(nodes.len() as u64);
@@ -478,6 +478,116 @@ fn randomize_embedding_store(es: &mut EmbeddingStore, rng: &mut impl Rng) {
         });
         norm = norm.sqrt();
         e.iter_mut().for_each(|ei| *ei /= norm);
+    }
+}
+
+trait Optimizer {
+    fn update(
+        &self, 
+        feature_embeddings: &EmbeddingStore,
+        grads: &mut HashMap<usize, Vec<f32>>,
+        alpha: f32,
+        t: f32
+    );
+
+}
+
+struct MomentumOptimizer {
+    gamma: f32,
+    mom: EmbeddingStore
+}
+
+impl MomentumOptimizer {
+
+    fn new(gamma: f32, dims: usize, length: usize) -> Self {
+        let mom = EmbeddingStore::new(length, dims, Distance::Cosine);
+        MomentumOptimizer {gamma, mom}
+    }
+}
+
+impl Optimizer for MomentumOptimizer {
+
+    fn update(
+        &self, 
+        feature_embeddings: &EmbeddingStore,
+        grads: &mut HashMap<usize, Vec<f32>>,
+        alpha: f32,
+        t: f32
+    ) {
+        for (feat_id, grad) in grads.drain() {
+
+            let emb = feature_embeddings.get_embedding_mut_hogwild(feat_id);
+            let mom = self.mom.get_embedding_mut_hogwild(feat_id);
+
+            if grad.iter().all(|gi| !gi.is_nan()) {
+                // Can get some nans in weird cases, such as the distance between
+                // a node and it's reconstruction when it shares all features.
+                // We just skip over those weird ones.
+                emb.iter_mut().zip(grad.iter().zip(mom.iter_mut())).for_each(|(ei, (gi, mi))| {
+                    *mi = self.gamma * *mi + *gi;
+                    *ei -= alpha * *mi;
+                });
+            }
+        }
+    }
+
+}
+
+struct AdamOptimizer {
+    beta_1: f32,
+    beta_2: f32,
+    eps: f32,
+    mom: EmbeddingStore,
+    var: EmbeddingStore
+}
+
+impl AdamOptimizer {
+    fn new(beta_1: f32, beta_2: f32, dims: usize, length: usize) -> Self {
+        let mom = EmbeddingStore::new(length, dims, Distance::Cosine);
+        let var = EmbeddingStore::new(length, dims, Distance::Cosine);
+        AdamOptimizer { beta_1, beta_2, mom, var, eps: 1e-8 }
+    }
+}
+
+impl Optimizer for AdamOptimizer {
+
+    fn update(
+        &self, 
+        feature_embeddings: &EmbeddingStore,
+        grads: &mut HashMap<usize, Vec<f32>>,
+        alpha: f32,
+        t: f32
+    ) {
+        for (feat_id, grad) in grads.drain() {
+
+            // Can get some nans in weird cases, such as the distance between
+            // a node and it's reconstruction when it shares all features.
+            // We just skip over those weird ones.
+            if grad.iter().all(|gi| !gi.is_nan()) {
+
+                // Update first order mean
+                let mom = self.mom.get_embedding_mut_hogwild(feat_id);
+                mom.iter_mut().zip(grad.iter()).for_each(|(m_i, g_i)| {
+                    let nm_i = self.beta_1 * *m_i + (1. - self.beta_1) * g_i;
+                    *m_i = nm_i / (1. - self.beta_1.powf(t));
+                });
+
+                // Update secord order variance 
+                let var = self.var.get_embedding_mut_hogwild(feat_id);
+                var.iter_mut().zip(grad.iter()).for_each(|(v_i, g_i)| {
+                    let nv_i = self.beta_2 * *v_i + (1. - self.beta_2) * g_i.powf(2.);
+                    *v_i = nv_i / (1. - self.beta_2.powf(t));
+                });
+
+                // Create the new grad and update
+                let emb = feature_embeddings.get_embedding_mut_hogwild(feat_id);
+                emb.iter_mut().zip(grad.iter()).zip(mom.iter().zip(var.iter())).for_each(|((e_i, g_i), (m_i, v_i))| {
+                    let g_i = m_i / (v_i.sqrt() + self.eps);
+                    *e_i -= alpha * g_i;
+                });
+
+            }
+        }
     }
 }
 
