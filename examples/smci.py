44df35e0325cbea993b1e2e4297bb6e764349677
Andrew Stanton <refefer@gmail.com>
2023-05-05 11:36:48 -0400

    example

---
+++ examples/smci.py
@@ -0,0 +1,38 @@
+import cloverleaf
+
+gb = cloverleaf.GraphBuilder()
+
+# 0 -- 1 -- 2 -- 3
+#      |    |
+# 4 -- 5 -- 6 -- 7
+for i in range(0, 3):
+    gb.add_edge(('node', str(i)), ('node', str(i+1)), 1, cloverleaf.EdgeType.Undirected)
+
+for i in range(4, 7):
+    gb.add_edge(('node', str(i)), ('node', str(i+1)), 1, cloverleaf.EdgeType.Undirected)
+
+gb.add_edge(('node', '1'), ('node', '5'), 1, cloverleaf.EdgeType.Undirected)
+gb.add_edge(('node', '2'), ('node', '6'), 1, cloverleaf.EdgeType.Undirected)
+
+graph = gb.build_graph()
+
+smci = cloverleaf.Smci(graph)
+
+smci.add_reward(('node', '0'), ('node', '3'), 2)
+smci.add_reward(('node', '1'), ('node', '5'), -2)
+#smci.add_reward(('node', '8'), ('node', '5'), 2)
+#smci.add_reward(('node', '8'), ('node', '4'), -2)
+
+new_graph = smci.optimize(
+    iterations=5,
+    num_walks=100,
+    alpha=0.0,
+    discount=0.9,
+    step_penalty=-0.1,
+    explore_pct=0.0,
+    compression=5.0,
+    restart_prob=1/3)
+
+new_graph.save("/tmp/here")
+print("done")
+
